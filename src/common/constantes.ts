539129447fd2ff16e7456b1153eb61957bf2e3bb
Jonas Garcia <48955198+JonasG4@users.noreply.github.com>
2023-10-12 23:27:06 -0600

    agregando filtros y busqueda por dui

--- src/common/constantes.ts
+++ src/common/constantes.ts
@@ -1,13 +1,13 @@
 export enum RabbitMQ {
   PersonaNaturalQueue = 'persona_natural',
-  
 }
 
 export enum PersonaNaturalMSG {
   CREATE = 'CREATE_PERSONA_NATURAL',
   FIND_ALL = 'FIND_PERSONA_NATURALES',
-  FIND_ONE = 'FIND__PERSONA_NATURAL',
- 
+  FIND_ONE = 'FIND_PERSONA_NATURAL',
+  FIND_BY_DUI = 'FIND_BY_DUI',
+  FIND_BY_FILTER = 'FIND_BY_FILTER',
 }
 
 export enum Folders {
--- src/persona-natural/persona-natural.controller.ts
+++ src/persona-natural/persona-natural.controller.ts
@@ -3,22 +3,27 @@ import { PersonaNaturalService } from './persona-natural.service';
 import { PersonaNaturalMSG } from 'src/common/constantes';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
-@Controller('persona-natural')
+@Controller()
 export class PersonaNaturalController {
-    constructor(private readonly perosonaNaturalService: PersonaNaturalService) {}
+  constructor(private readonly personaNaturalService: PersonaNaturalService) {}
 
   @MessagePattern(PersonaNaturalMSG.CREATE)
   async create(@Payload() payload: any) {
-    return await this.perosonaNaturalService.create(payload);
+    return await this.personaNaturalService.create(payload);
   }
 
   @MessagePattern(PersonaNaturalMSG.FIND_ALL)
-  async findAll() {
-    return this.perosonaNaturalService.findAll();
+  async findAll(@Payload() filters: any) {
+    return this.personaNaturalService.findAll(filters);
   }
 
   @MessagePattern(PersonaNaturalMSG.FIND_ONE)
   async findOne(@Payload() id: number) {
-    return this.perosonaNaturalService.findOne(id);
+    return this.personaNaturalService.findOne(id);
+  }
+
+  @MessagePattern(PersonaNaturalMSG.FIND_BY_DUI)
+  async findByDui(@Payload() dui: string) {
+    return this.personaNaturalService.findByDui(dui);
   }
 }
